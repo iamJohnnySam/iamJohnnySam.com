0a20861cd718138c1cf4f7c71158807f05844daf
iamJohnnySam <51484429+iamJohnnySam@users.noreply.github.com>
2021-09-25 13:35:26 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,501 @@
+	<!----- About----->
+	<section class="about" id="about">
+		<h1>John Samarasinghe</h1>BEng(Hons), MSc, MIET
+		<p>A data driven and results oriented Mechatronic Engineer with more than 5 years of experience in Manufacturing and R&amp;D. Demonstrated a strong background in Mechatronics, control systems and programming backed up with lean best practices in designing machinery from end-to-end. A collaborative team member and confident team leader in delivering projects on-time with the highest quality.</p>
+		
+		<div class="row">
+			<div class="about-col">
+				<h3>Current Designation</h3>
+				<p>Senior Business Analyst - Autonomation<br>
+					MAS Active Pvt Ltd, Sri Lanka<br>
+					2021-01-01
+				</p>
+			</div>
+			<div class="about-col">
+				<h3>Highest Education</h3>
+				<p>Master of Science<br>
+					Electronics and Automation<br>
+					University of Moratuwa, Sri Lanka<br>
+					2021
+				</p>
+			</div>
+			<div class="about-col">
+				<h3>Professional Membership</h3>
+				<p>MIET - Member<br>
+					The Institution of Engineering and Technology<br>
+					2016
+				</p>
+			</div>
+		</div>
+		<a href="/cv/JohnSamarasingheCV.pdf" download class="box-btn white-btn">Download my CV</a>
+	</section>
+	<section class="skills">
+		<h2>Professional Skills</h2>
+		
+		
+		<div class="skillcat">
+			<h3>Electronics and Embedded Systems</h3>
+			
+			<div class="row">
+				<div class="skill-col">
+					<table>
+						<tr>
+							<td colspan="3">Micro-controller and Processor</td>
+							<td class="skillbarcol"><div class="skillbar"><div class="percent p70">70%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td class="skillimage"><img src="website_images/skillmicroc.png" alt="logo"></td>
+							<td class="skillname">Microchip</td>
+							<td><div class="skillbar"><div class="percent p60">60%</div></div></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><img src="website_images/skillc.png" alt="logo"></td>
+							<td class="skillname">C Programming</td>
+							<td><div class="skillbar"><div class="percent p70">70%</div></div></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><img src="website_images/skillarduino.jpg" alt="logo"></td>
+							<td class="skillname">Arduino<br>ESP32 | ESP8266</td>
+							<td><div class="skillbar"><div class="percent p70">70%</div></div></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><img src="website_images/skillrpi.png" alt="logo"></td>
+							<td class="skillname">Raspbian | Raspberry Pi</td>
+							<td><div class="skillbar"><div class="percent p60">60%</div></div></td>
+						</tr>
+					</table>
+					
+					<table>
+						<tr>
+							<td colspan="3">PLC Implementation and Programming</td>
+							<td class="skillbarcol"><div class="skillbar"><div class="percent p80">80%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td class="skillimage"><img src="website_images/skillMitsubishi.png" alt="logo"></td>
+							<td class="skillname">Mitsubishi PLC</td>
+							<td><div class="skillbar"><div class="percent p80">80%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td><img src="website_images/skillsiemens.jpg" alt="logo"></td>
+							<td class="skillname">Siemens PLC</td>
+							<td><div class="skillbar"><div class="percent p60">60%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td><img src="website_images/skillxinje.png" alt="logo"></td>
+							<td class="skillname">Xinje PLC</td>
+							<td><div class="skillbar"><div class="percent p80">80%</div></div></td>
+						</tr>
+					</table>
+					
+					<table>
+						<tr>
+							<td colspan="3">Electronic | Power Electronic Design</td>
+							<td class="skillbarcol"><div class="skillbar"><div class="percent p70">70%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td class="skillimage"><img src="website_images/skillmultisim.jpg" alt="logo"></td>
+							<td class="skillname">Multisim</td>
+							<td><div class="skillbar"><div class="percent p60">60%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td><img src="website_images/skillltspice.jpg" alt="logo"></td>
+							<td class="skillname">LT Spice</td>
+							<td><div class="skillbar"><div class="percent p60">60%</div></div></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><img src="website_images/skillproteus.png" alt="logo"></td>
+							<td class="skillname">Proteus</td>
+							<td><div class="skillbar"><div class="percent p60">60%</div></div></td>
+						</tr>
+					</table>
+					
+					<table>
+						<tr>
+							<td colspan="3">PCB Design</td>
+							<td class="skillbarcol"><div class="skillbar"><div class="percent p60">60%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td class="skillimage"><img src="website_images/skilleasyeda.png" alt="logo"></td>
+							<td class="skillname">Easy EDA</td>
+							<td><div class="skillbar"><div class="percent p70">70%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td><img src="website_images/skillaltium.png" alt="logo"></td>
+							<td class="skillname">Altium</td>
+							<td><div class="skillbar"><div class="percent p60">60%</div></div></td>
+						</tr>
+					</table>
+					
+				</div>
+				<div class="skill-col">
+					<table>
+						<tr>
+							<td colspan="3">Control Systems</td>
+							<td class="skillbarcol"><div class="skillbar"><div class="percent p70">70%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td class="skillimage"><img src="website_images/skillmatlab.jpg" alt="logo"></td>
+							<td class="skillname">Classical Control<br></td>
+							<td><div class="skillbar"><div class="percent p80">80%</div></div></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><img src="website_images/skillmatlab.jpg" alt="logo"></td>
+							<td class="skillname">State Space Control</td>
+							<td><div class="skillbar"><div class="percent p70">70%</div></div></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><img src="website_images/skillmatlab.jpg" alt="logo"></td>
+							<td class="skillname">Adaptive Control</td>
+							<td><div class="skillbar"><div class="percent p60">60%</div></div></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><img src="website_images/skillmatlab.jpg" alt="logo"></td>
+							<td class="skillname">Machine Learning</td>
+							<td><div class="skillbar"><div class="percent p70">70%</div></div></td>
+						</tr>
+					</table>
+					
+					<table>
+						<tr>
+							<td colspan="3">Robotics</td>
+							<td class="skillbarcol"><div class="skillbar"><div class="percent p70">70%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td class="skillimage"><img src="website_images/skillmatlab.jpg" alt="logo"></td>
+							<td class="skillname">Robotic Transofrmations</td>
+							<td><div class="skillbar"><div class="percent p70">70%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td class="skillimage"><img src="website_images/skillmatlab.jpg" alt="logo"></td>
+							<td class="skillname">Kinematics &amp; Inverse Kinematics</td>
+							<td><div class="skillbar"><div class="percent p70">70%</div></div></td>
+						</tr>
+					</table>
+					
+					
+				</div>
+			</div>
+		</div>
+		
+		
+		<div class="skillcat">
+			<h3>Robotics and Solid Modelling | Geometric Dimensioning and Tolerencing</h3>
+			
+			<div class="row">
+				<div class="skill-col">
+					<table>
+						<tr>
+							<td colspan="3">2D Modeling</td>
+							<td class="skillbarcol"><div class="skillbar"><div class="percent p70">70%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td class="skillimage"><img src="website_images/skillautocad.jpg" alt="logo"></td>
+							<td class="skillname">AutoCAD<br>AutoCAD Electrical</td>
+							<td><div class="skillbar"><div class="percent p60">60%</div></div></td>
+						</tr>
+					</table>
+				</div>
+				
+				<div class="skill-col">
+					<table>
+						<tr>
+							<td colspan="3">Solid Modeling | Drawings</td>
+							<td class="skillbarcol"><div class="skillbar"><div class="percent p80">80%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td class="skillimage"><img src="website_images/skillsolidworks.png" alt="logo"></td>
+							<td class="skillname">Solidworks</td>
+							<td><div class="skillbar"><div class="percent p80">80%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td><img src="website_images/skillsolidworks.png" alt="logo"></td>
+							<td class="skillname">Stress Simulations</td>
+							<td><div class="skillbar"><div class="percent p70">70%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td><img src="website_images/skillsolidworks.png" alt="logo"></td>
+							<td class="skillname">GD&amp;T</td>
+							<td><div class="skillbar"><div class="percent p70">70%</div></div></td>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+		
+		
+		<div class="skillcat">
+			<h3>Software Programming</h3>
+			
+			<div class="row">
+				<div class="skill-col">
+					
+					<table>
+						<tr>
+							<td colspan="3">Programming</td>
+							<td class="skillbarcol"><div class="skillbar"><div class="percent p70">70%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td class="skillimage"><img src="website_images/skillc.png" alt="logo"></td>
+							<td class="skillname">C Programming</td>
+							<td><div class="skillbar"><div class="percent p70">70%</div></div></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><img src="website_images/skillcpp.png" alt="logo"></td>
+							<td class="skillname">C++ Programming</td>
+							<td><div class="skillbar"><div class="percent p50">50%</div></div></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><img src="website_images/skillpython.jpg" alt="logo"></td>
+							<td class="skillname">Python 3 Programming</td>
+							<td><div class="skillbar"><div class="percent p70">70%</div></div></td>
+						</tr>
+					</table>
+
+					<table>
+						<tr>
+							<td colspan="3">Digital System Design</td>
+							<td class="skillbarcol"><div class="skillbar"><div class="percent p70">70%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td class="skillimage"><img src="website_images/skillxilinx.jpg" alt="logo"></td>
+							<td class="skillname">Verilog | Xilinx</td>
+							<td><div class="skillbar"><div class="percent p70">70%</div></div></td>
+						</tr>
+					</table>
+
+					
+
+					<table>
+						<tr>
+							<td colspan="3">Web Design</td>
+							<td class="skillbarcol"><div class="skillbar"><div class="percent p60">60%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td class="skillimage"><img src="website_images/skillhtml.png" alt="logo"></td>
+							<td class="skillname">HTML</td>
+							<td><div class="skillbar"><div class="percent p60">60%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td><img src="website_images/skillcss.png" alt="logo"></td>
+							<td class="skillname">CSS</td>
+							<td><div class="skillbar"><div class="percent p60">60%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td><img src="website_images/skillphp.png" alt="logo"></td>
+							<td class="skillname">PHP</td>
+							<td><div class="skillbar"><div class="percent p40">40%</div></div></td>
+						</tr>
+					</table>
+
+					
+				</div>
+				<div class="skill-col">
+					<table>
+						<tr>
+							<td colspan="3">Mathematical Simulations</td>
+							<td class="skillbarcol"><div class="skillbar"><div class="percent p60">60%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td class="skillimage"><img src="website_images/skillmatlab.jpg" alt="logo"></td>
+							<td class="skillname">MATLAB and Simulink</td>
+							<td><div class="skillbar"><div class="percent p60">60%</div></div></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><img src="website_images/skilllabview.png" alt="logo"></td>
+							<td class="skillname">LabVIEW</td>
+							<td><div class="skillbar"><div class="percent p50">50%</div></div></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><img src="website_images/skillproteus.png" alt="logo"></td>
+							<td class="skillname">Proteus</td>
+							<td><div class="skillbar"><div class="percent p60">60%</div></div></td>
+						</tr>
+					</table>
+					
+					<table>
+						<tr>
+							<td colspan="3">Graphical User Interface Design</td>
+							<td class="skillbarcol"><div class="skillbar"><div class="percent p50">50%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td class="skillimage"><img src="website_images/skillkivy.jpg" alt="logo"></td>
+							<td class="skillname">Kivy NUI</td>
+							<td><div class="skillbar"><div class="percent p50">50%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td><img src="website_images/skillpowerapps.png" alt="logo"></td>
+							<td class="skillname">Microsoft Power Apps</td>
+							<td><div class="skillbar"><div class="percent p70">70%</div></div></td>
+						</tr>
+					</table>
+					
+				</div>
+			</div>
+		</div>
+		
+		<div class="skillcat">
+			<h3>Data Analytics</h3>
+			
+			<div class="row">
+				<div class="skill-col">
+					
+					<table>
+						<tr>
+							<td colspan="3">Machine Learning</td>
+							<td class="skillbarcol"><div class="skillbar"><div class="percent p50">50%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td class="skillimage"><img src="website_images/skillpython.jpg" alt="logo"></td>
+							<td class="skillname">Classical Machine Learning with Python</td>
+							<td><div class="skillbar"><div class="percent p60">60%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td><img src="website_images/skillscikit.png" alt="logo"></td>
+							<td class="skillname">Classical ML: Scikit learn</td>
+							<td><div class="skillbar"><div class="percent p70">70%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td><img src="website_images/skillkeras.png" alt="logo"></td>
+							<td class="skillname">Deep Machine Learning with Python | Keras</td>
+							<td><div class="skillbar"><div class="percent p50">50%</div></div></td>
+						</tr>
+					</table>
+
+					
+				</div>
+				<div class="skill-col">
+					<table>
+						<tr>
+							<td colspan="3">Robotic Process Automation</td>
+							<td class="skillbarcol"><div class="skillbar"><div class="percent p80">80%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td class="skillimage"><img src="website_images/skillpowerautomate.png" alt="logo"></td>
+							<td class="skillname">Microsoft Power Automate (Flow)</td>
+							<td><div class="skillbar"><div class="percent p80">80%</div></div></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><img src="website_images/skillpython.jpg" alt="logo"></td>
+							<td class="skillname">Automating with Python</td>
+							<td><div class="skillbar"><div class="percent p70">70%</div></div></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><img src="website_images/skillpowerapps.png" alt="logo"></td>
+							<td class="skillname">Microsoft Power Apps</td>
+							<td><div class="skillbar"><div class="percent p70">70%</div></div></td>
+						</tr>
+					</table>
+					
+					<table>
+						<tr>
+							<td colspan="3">Data Analytics and Visualization</td>
+							<td class="skillbarcol"><div class="skillbar"><div class="percent p80">80%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td class="skillimage"><img src="website_images/skillpowerbi.png" alt="logo"></td>
+							<td class="skillname">Microsoft PowerBI</td>
+							<td><div class="skillbar"><div class="percent p90">90%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td><img src="website_images/skilldatastudio.svg" alt="logo"></td>
+							<td class="skillname">Google Data Studio</td>
+							<td><div class="skillbar"><div class="percent p50">50%</div></div></td>
+						</tr>
+					</table>
+
+				</div>
+			</div>
+		</div>
+		
+		<div class="skillcat">
+			<h3>Management</h3>
+			
+			<div class="row">
+				<div class="skill-col">
+					<table>
+						<tr>
+							<td colspan="3">Project Management</td>
+							<td class="skillbarcol"><div class="skillbar"><div class="percent p60">60%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td class="skillimage"><img src="website_images/skillpmp.png" alt="logo"></td>
+							<td class="skillname">Project Management</td>
+							<td><div class="skillbar"><div class="percent p60">60%</div></div></td>
+						</tr>
+					</table>
+				</div>
+				
+				<div class="skill-col">
+					<table>
+						<tr>
+							<td colspan="3">Lean Manufacturing</td>
+							<td class="skillbarcol"><div class="skillbar"><div class="percent p60">60%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td class="skillimage"><img src="website_images/person.png" alt="logo"></td>
+							<td class="skillname">Work Study</td>
+							<td><div class="skillbar"><div class="percent p60">60%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td><img src="website_images/person.png" alt="logo"></td>
+							<td class="skillname">Production Engineering</td>
+							<td><div class="skillbar"><div class="percent p60">60%</div></div></td>
+						</tr>
+						<tr>
+							<td width="5%"></td>
+							<td><img src="website_images/person.png" alt="logo"></td>
+							<td class="skillname">Lean Thinking</td>
+							<td><div class="skillbar"><div class="percent p60">60%</div></div></td>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+		
+		
+	</section>
